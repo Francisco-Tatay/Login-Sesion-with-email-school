a62f085fd2e2b14793c0188dc158d2e9bfaa8b51
Phoenix1203736 <pacotorrestatay8@gmail.com>
2026-02-09 09:04:00 -0600

    first update windows

--- Backend/Model/User.js
+++ Backend/Model/User.js
@@ -1,9 +1,32 @@
 class User {
-#id;
-#name
-#email;
-#password;
+  #id;
+  #name;
+  #email;
+  #password;
 
-
-
+  get id() {
+    return this.#id;
+  }
+  set id(id) {
+    this.#id = id;
+  }
+  get name() {
+    return this.#name;
+  }
+  set name(name) {
+    this.#name = name;
+  }
+  get email() {
+    return this.#email;
+  }
+  set email(email) {
+    this.#email = email;
+  }
+  get password(){
+    return this.#password;
+  }
+  set password(password){
+    this.#password = password;
+  }
+  
 }
--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -1,7 +1,13 @@
 import { Router } from "express";
 const router = Router();
-router.get('/',(request,response)=>{
-  response.send({message:"Hello from routes!"});
-})
+router.get(
+  "/",
+  (request, response) => {
+    response.send({ message: "Hello from routes!" });
+  },
+  router.get("/test", (request, response) => {
+    response.send({message: "Hello from test route!"});
+  }),
+);
 
 export default router;
